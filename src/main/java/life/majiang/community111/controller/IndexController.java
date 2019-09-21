4e424cfd3d10bc914932398aa5fa13da6204f44e
叶金勇 <365501916@qq.com>
2019-09-21 10:35:35 +0800

    t

---
+++ src/main/java/life/majiang/community111/controller/IndexController.java
@@ -0,0 +1,17 @@
+package life.majiang.community111.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @description:
+ * @author: Ye YinYong
+ * @create: 2019-09-20 21:30
+ **/
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
