dea211efd6012542162d78525c89c02d654bf7af
叶金勇 <365501916@qq.com>
2019-09-22 16:50:04 +0800

    add check cookie

--- src/main/java/life/majiang/community2/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community2/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -39,7 +40,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam("code") String code,
                            @RequestParam("state") String state,
-                           HttpServletRequest request) {
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -57,8 +58,7 @@ public class AuthorizeController {
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            //登录成功，写session
-            request.getSession().setAttribute("user", githubUser);
+            response.addCookie(new Cookie("token", user.getToken()));
             return "redirect:/";
         } else {
             //登录失败，重新登录
